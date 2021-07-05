25bd7a10b4d0e63219ced8d8d52e479ef2361d93
kchomg <kchomg@naver.com>
2021-07-05 13:05:22 +0900

    Add view Diddisappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -13,7 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        <#code#>
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
         <#code#>
     }
 
